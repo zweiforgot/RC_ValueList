dadcdd506c025e1648a266eff18021489cac04d6
zwei <wyatthjones@outlook.com>
2022-11-30 23:00:37 -0500

    site

---
+++ resources/index.js
@@ -0,0 +1,20 @@
+// const { GoogleSpreadsheet } = require('google-spreadsheet');
+// const sheets = "AIzaSyC_OioNtgoiZS-Z_YnipsmXHWfK44jh_nk"
+// const doc = new GoogleSpreadsheet('1Jdu8fp8qWNcE4BtZsq4_BZi-4AhfeljvNNzPeALW6Xg');
+
+// doc.useApiKey(sheets)
+
+// const loadDoc = async () => {
+//     await doc.loadInfo();
+
+//     console.log(doc.title);
+//     await doc.sheetsByIndex[0];
+//     let sheet = doc.sheetsByIndex[0];
+//     sheet = sheet.getCellByA1("B2")
+//     setTimeout(() => {
+//         console.log(sheet);
+//     }, 1000);
+   
+// };
+
+// loadDoc();
